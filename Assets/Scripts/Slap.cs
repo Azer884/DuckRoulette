5660e21e0ce478755ea8da71d7061bfdf2db92a4
Azer Hamdaoui <84522133+Azer884@users.noreply.github.com>
2024-09-11 23:59:09 +0100

    Fix Bugs

--- Assets/Scripts/Slap.cs
+++ Assets/Scripts/Slap.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using UnityEngine;
+using Unity.Netcode;
 
-class Slap : MonoBehaviour 
+public class Slap : NetworkBehaviour 
 {
     private PlayerInput inputActions;
     [SerializeField]private Transform slapArea;
@@ -9,8 +10,16 @@ class Slap : MonoBehaviour
     [SerializeField]private float slapCoolDown = 1f;
     [SerializeField] private Animator[] animators;
     [SerializeField] private LayerMask otherPlayers;
+    [SerializeField] private float slapForce = 5.0f;
+    private Collider[] slappedPlayers;
     private bool canSlap = true;
 
+    public override void OnNetworkSpawn()
+    {
+        if (!IsOwner) enabled = false;
+
+        base.OnNetworkSpawn();
+    }
     private void Awake() {
         inputActions = new PlayerInput();
     }
@@ -23,14 +32,11 @@ class Slap : MonoBehaviour
     private void Update() {
         if (inputActions.PlayerControls.Slap.triggered && canSlap)
         {
-            if(Physics.CheckSphere(slapArea.position, slapRaduis, otherPlayers))
-            {
-
-            }
             foreach (Animator anim in animators)
             {
                 anim.SetTrigger("Slap");
             }
+            TryToSlap();
 
             canSlap = false;
             StartCoroutine(Timer(slapCoolDown));
@@ -38,6 +44,7 @@ class Slap : MonoBehaviour
     }
     private void OnDrawGizmos()
     {
+        Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(slapArea.position, slapRaduis);
     }
 
@@ -45,5 +52,48 @@ class Slap : MonoBehaviour
     {
         yield return new WaitForSeconds(waitTime);
         canSlap = true;
+    }
+    private void TryToSlap()
+    {
+        slappedPlayers = Physics.OverlapSphere(slapArea.position, slapRaduis, otherPlayers);
+
+        if (slappedPlayers != null && slappedPlayers.Length > 1)
+        {
+            GameObject slappedPlayer = slappedPlayers[0].gameObject;
+            if (slappedPlayer != gameObject)
+            {
+                SlapPlayer(slappedPlayer);
+            }
+            else
+            {
+                slappedPlayer = slappedPlayers[1].gameObject;
+                SlapPlayer(slappedPlayer);
+            }
+        }
+    }
+
+    private void SlapPlayer(GameObject player)
+    {
+        CharacterController targetController = player.GetComponent<CharacterController>();
+        if (targetController != null)
+        {
+            Vector3 slapDirection = (player.transform.position - transform.position).normalized;
+            Vector3 slapVelocity = slapDirection * slapForce;
+            
+            StartCoroutine(ApplySlap(targetController, slapVelocity));
+        }
+    }
+
+    private IEnumerator ApplySlap(CharacterController targetController, Vector3 slapVelocity)
+    {
+        float duration = 0.2f;
+        float timer = 0;
+
+        while (timer < duration)
+        {
+            targetController.Move(slapVelocity * Time.deltaTime);
+            timer += Time.deltaTime;
+            yield return null;
+        }
     }
 }
